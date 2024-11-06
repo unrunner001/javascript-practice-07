23e9e792ac908b4a579a3590bf733699f167bf92
sergei sumskoi <oceanbreeze018@ukr.net>
2024-11-07 00:13:19 +0200

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,29 @@
+'use strict';
+
+class Storage {
+  #item;
+  constructor(item) {
+    this.#item = item;
+  }
+  getItems() {
+    return this.#item;
+  }
+  addItem(newItem) {
+    this.#item.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    this.#item = this.#item.filter(item => item !== itemToRemove);
+  }
+}
+
+const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+
+storage.addItem('Droid');
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem('Prolonger');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem('Scaner');
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
